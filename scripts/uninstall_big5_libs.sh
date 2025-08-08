ad9eddb2052952cc89295b1aea608b5d93896370
rsoleyma <ramin.soleymani@bsc.es>
2025-08-09 00:18:20 +0200

    add small things/clean stage

---
+++ scripts/uninstall_big5_libs.sh
@@ -0,0 +1,3 @@
+# uninstall the big5 libraries (databases and python-tools) so that we can add the source directly as dependency
+uv pip uninstall big5_databases big5-python-tools
+# todo, change prepare.sh
